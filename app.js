415386c2d98439c097b6166f7a184c0a4f50304b
Javier Salinas <javiersalinasdev@gmail.com>
2023-10-11 22:08:04 -0300

    Botones aumentar / disminuir | Centrado de contenedores todo con bootstrap :v | Codigo JS para interaccion

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //obtengo todos los elementos con clase ".cart-item-count"
 const elementos = document.querySelectorAll('.cart-item-count');
+const aumentarButtons = document.querySelectorAll('.aumentar');
+const disminuirButtons = document.querySelectorAll('.disminuir');
+
 
 
 function calcularResumen() {
@@ -17,35 +20,70 @@ function calcularResumen() {
     };
 }
 
-
-elementos.forEach(elemento => {
-    elemento.addEventListener('change', (event) => {
-
-        // Encuentra el elemento padre mas cercano con la clase "tr-detail"
-        const row = elemento.closest('.tr-detail');
-        //Encuentra el item donde esta el precio
-        const total_item = row.querySelector('.cart-item-price');
-        //Encuentra el elemento "p-td-price" dentro de la fila
-        const priceElement = row.querySelector('.p-td-price');
-        const price = parseFloat(priceElement.textContent);
-        const count_items = parseFloat(elemento.value);
-        total_item.textContent = (price * count_items);
+document.addEventListener('DOMContentLoaded', function () {
 
 
-        //actualizo resumen
-        const quant = document.getElementById('quant');
-        const total = document.getElementById('total');
-        const subtotal = document.getElementById('subtotal');
-        const envio = document.getElementById('envio');
 
-        let total_value = parseFloat(total.textContent);
-        let subtotal_value = parseFloat(subtotal.textContent);
-        let envio_value = parseFloat(envio.textContent)
-        let resumen = calcularResumen();
-        quant.textContent = resumen.suma;
-        subtotal_value = resumen.subtotal;
-        subtotal.textContent = subtotal_value;
-        total_value = subtotal_value + envio_value;
-        total.textContent = total_value;
+    elementos.forEach(elemento => {
+        elemento.addEventListener('change', (event) => {
+            // Encuentra el elemento padre mas cercano con la clase "tr-detail"
+            const row = elemento.closest('.tr-detail');
+            //Encuentra el item donde esta el precio
+            const total_item = row.querySelector('.cart-item-price');
+            //Encuentra el elemento "p-td-price" dentro de la fila
+            const priceElement = row.querySelector('.p-td-price');
+            const price = parseFloat(priceElement.textContent);
+            const count_items = parseFloat(elemento.value);
+            total_item.textContent = (price * count_items);
+
+
+            //actualizo resumen
+            const quant = document.getElementById('quant');
+            const total = document.getElementById('total');
+            const subtotal = document.getElementById('subtotal');
+            const envio = document.getElementById('envio');
+
+            let total_value = parseFloat(total.textContent);
+            let subtotal_value = parseFloat(subtotal.textContent);
+            let envio_value = parseFloat(envio.textContent)
+            let resumen = calcularResumen();
+            quant.textContent = resumen.suma;
+            subtotal_value = resumen.subtotal;
+            subtotal.textContent = subtotal_value;
+            total_value = subtotal_value + envio_value;
+            total.textContent = total_value;
+        });
+    });
+
+
+    aumentarButtons.forEach(aumentarButton => {
+        aumentarButton.addEventListener('click', function () {
+            const cartItemCountElement = aumentarButton.closest('.input-group').querySelector('.cart-item-count');
+            let valor = parseInt(cartItemCountElement.value);
+    
+            if (valor < parseInt(cartItemCountElement.getAttribute('max'))) {
+                valor++;
+                cartItemCountElement.value = valor;
+                // Llamo manualmente al evento change! No se si es correcto
+                cartItemCountElement.dispatchEvent(new Event('change'));
+            }
+        });
+    });
+    
+    disminuirButtons.forEach(disminuirButton => {
+        disminuirButton.addEventListener('click', function () {
+            const cartItemCountElement = disminuirButton.closest('.input-group').querySelector('.cart-item-count');
+            let valor = parseInt(cartItemCountElement.value);
+    
+            if (valor > parseInt(cartItemCountElement.getAttribute('min'))) {
+                valor--;
+                cartItemCountElement.value = valor;
+                // Llamo manualmente al evento change! No se si es correcto
+                cartItemCountElement.dispatchEvent(new Event('change'));
+            }
+        });
     });
 });
+
+
+
