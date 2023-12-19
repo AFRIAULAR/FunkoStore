03b41c0dd0316d1b37a8c697ee0926a96b7c657a
Anabella Bernachea <nekochan276@gmail.com>
2023-12-18 23:26:27 -0300

    logica para el creado y editado de items en admin

---
+++ src/services/userService.js
@@ -0,0 +1,31 @@
+const user = require('../models/login')
+
+const crearUsuario = async (UserInfo) =>{
+    const creado = await user.crearUsuario(UserInfo);
+    return creado;
+};
+
+const verificarUser = async (user_id) => {
+    const verificado = await user.verificarUser(user_id);
+    return verificado;
+};
+
+const getUserRole = async(user_id) => {
+    const verificado = await user.getUserRole(user_id);
+}
+
+const getUserByEmail = async (email) => {
+    return user_info = await user.getUserByEmail(email);
+};
+
+const registerUser = async (UserInfo) => {
+    return response = await user.registerUser(UserInfo); 
+};
+
+module.exports = {
+    crearUsuario,
+    verificarUser,
+    getUserRole,
+    getUserByEmail,
+    registerUser
+}
